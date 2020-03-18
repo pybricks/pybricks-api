65deb297dd0656a7b902ef7d51caa90a639e088e
David Lechner <david@pybricks.com>
2020-03-18 18:10:18 -0500

    api: remove Port subclass from hubs
    This was removed in the pybricks-micropython implementation.

--- pybricks/hubs.py
+++ pybricks/hubs.py
@@ -2,14 +2,12 @@
 # Copyright (c) 2018-2020 The Pybricks Authors
 
 """LEGO® Programmable Hubs."""
-from enum import Enum
 from .builtins import Speaker, Battery, ColorLight, KeyPad
 from .media.ev3dev import Image
 
 
 class EV3Brick:
     """LEGO® MINDSTORMS® EV3 Brick."""
-    Port = Enum('Port', ['A', 'B', 'C', 'D', 'S1', 'S2', 'S3', 'S4'])
     screen = Image('_screen_')
     speaker = Speaker()
     battery = Battery()
@@ -19,13 +17,11 @@ class EV3Brick:
 
 class MoveHub:
     """LEGO® Powered Up Move Hub."""
-    Port = Enum('Port', ['A', 'B', 'C', 'D'])
     battery = Battery()
     light = ColorLight()
 
 
 class CityHub:
     """LEGO® Powered Up City Hub."""
-    Port = Enum('Port', ['A', 'B'])
     battery = Battery()
     light = ColorLight()
