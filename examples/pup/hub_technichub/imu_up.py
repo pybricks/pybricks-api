4b5b97bbb2de77f66270caee39add36a8ab096f1
Laurens Valk <laurens@pybricks.com>
2021-01-20 11:13:24 +0100

    examples/pup: add imu up() example

---
+++ examples/pup/hub_technichub/imu_up.py
@@ -0,0 +1,29 @@
+from pybricks.hubs import TechnicHub
+from pybricks.parameters import Color, Side
+from pybricks.tools import wait
+
+# Initialize the hub.
+hub = TechnicHub()
+
+# Define colors for each side in a dictionary.
+SIDE_COLORS = {
+    Side.TOP: Color.RED,
+    Side.BOTTOM: Color.BLUE,
+    Side.LEFT: Color.GREEN,
+    Side.RIGHT: Color.YELLOW,
+    Side.FRONT: Color.MAGENTA,
+    Side.BACK: Color.BLACK,
+}
+
+# Keep updating the color based on detected up side.
+while True:
+
+    # Check which side of the hub is up.
+    up_side = hub.imu.up()
+
+    # Change the color based on the side.
+    hub.light.on(SIDE_COLORS[up_side])
+
+    # Also print the result.
+    print(up_side)
+    wait(50)
