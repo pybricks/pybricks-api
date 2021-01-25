06e01e5ac88cb764d7750a74ea4b1109d39f62f2
David Lechner <david@pybricks.com>
2021-01-25 17:53:27 -0600

    doc/ide: use sphinx.ext.imgmath
    MathJax uses a CDN to to distribute the package. So in order to use the
    docs offline, we would have to include our own copy. But it is a bit
    large (50-ish MB), so it is easier to use sphinx.ext.imgmath instead
    to generate svg images.
    
    The situation might be different with MathJax v3, but that requires
    Sphinx v4.

--- doc/ide/conf.py
+++ doc/ide/conf.py
@@ -19,6 +19,14 @@ html_show_copyright = False
 html_show_sphinx = False
 html_css_files = ['css/ide.css']
 
+imgmath_image_format = 'svg'
+imgmath_use_preview = True  # requires Sphinx v3
+imgmath_latex_preamble = r'''
+\usepackage{newtxsf}
+'''
+
 exec(open(os.path.abspath("../common/conf.py")).read())
 
+extensions.remove('sphinx.ext.mathjax')  # noqa F821
+extensions.append('sphinx.ext.imgmath')  # noqa F821
 html_theme_options['prev_next_buttons_location'] = None  # noqa F821
