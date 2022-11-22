7dd3edaee4dd7c589b4c6306a9fd2e1b779dcb79
Laurens Valk <laurens@pybricks.com>
2022-11-22 14:20:23 +0100

    jedi/tests: Make PrimeHub the same as InventorHub.

--- jedi/tests/test_complete_inventor_hub.py
+++ jedi/tests/test_complete_inventor_hub.py
@@ -27,113 +27,16 @@ def _create_snippet(line: str) -> str:
     return "\n".join((IMPORT, CREATE_INSTANCE, line))
 
 
-def test_hub_dot():
-    line = "hub."
-    code = _create_snippet(line)
-    completions: list[CompletionItem] = json.loads(complete(code, 3, len(line) + 1))
-    assert [c["insertText"] for c in completions] == [
-        "battery",
-        "buttons",
-        "charger",
-        "display",
-        "imu",
-        "light",
-        "speaker",
-        "system",
-    ]
+# Everything else is the same as the Prime Hub.
 
-
-def test_hub_dot_battery_dot():
-    line = "hub.battery."
-    code = _create_snippet(line)
-    completions: list[CompletionItem] = json.loads(complete(code, 3, len(line) + 1))
-    assert [c["insertText"] for c in completions] == [
-        "current",
-        "voltage",
-    ]
-
-
-def test_hub_dot_buttons_dot():
-    line = "hub.buttons."
-    code = _create_snippet(line)
-    completions: list[CompletionItem] = json.loads(complete(code, 3, len(line) + 1))
-    assert [c["insertText"] for c in completions] == [
-        "pressed",
-    ]
-
-
-def test_hub_dot_charger_dot():
-    line = "hub.charger."
-    code = _create_snippet(line)
-    completions: list[CompletionItem] = json.loads(complete(code, 3, len(line) + 1))
-    assert [c["insertText"] for c in completions] == [
-        "connected",
-        "current",
-        "status",
-    ]
-
-
-def test_hub_dot_display_dot():
-    line = "hub.display."
-    code = _create_snippet(line)
-    completions: list[CompletionItem] = json.loads(complete(code, 3, len(line) + 1))
-    assert [c["insertText"] for c in completions] == [
-        "animate",
-        "char",
-        "image",
-        "number",
-        "off",
-        "orientation",
-        "pixel",
-        "text",
-    ]
-
-
-def test_hub_dot_imu_dot():
-    line = "hub.imu."
-    code = _create_snippet(line)
-    completions: list[CompletionItem] = json.loads(complete(code, 3, len(line) + 1))
-    assert [c["insertText"] for c in completions] == [
-        "acceleration",
-        "angular_velocity",
-        "heading",
-        "reset_heading",
-        "tilt",
-        "up",
-    ]
-
-
-def test_hub_dot_light_dot():
-    line = "hub.light."
-    code = _create_snippet(line)
-    completions: list[CompletionItem] = json.loads(complete(code, 3, len(line) + 1))
-    assert [c["insertText"] for c in completions] == [
-        "animate",
-        "blink",
-        "off",
-        "on",
-    ]
-
-
-def test_hub_dot_speaker_dot():
-    line = "hub.speaker."
-    code = _create_snippet(line)
-    completions: list[CompletionItem] = json.loads(complete(code, 3, len(line) + 1))
-    assert [c["insertText"] for c in completions] == [
-        "beep",
-        "play_notes",
-        "volume",
-    ]
-
-
-def test_hub_dot_system_dot():
-    line = "hub.system."
-    code = _create_snippet(line)
-    completions: list[CompletionItem] = json.loads(complete(code, 3, len(line) + 1))
-    assert [c["insertText"] for c in completions] == [
-        "name",
-        "reset_reason",
-        "set_stop_button",
-        "shutdown",
-        "storage",
-    ]
+from test_complete_prime_hub import (  # noqa F401
+    test_hub_dot,
+    test_hub_dot_battery_dot,
+    test_hub_dot_buttons_dot,
+    test_hub_dot_charger_dot,
+    test_hub_dot_display_dot,
+    test_hub_dot_imu_dot,
+    test_hub_dot_light_dot,
+    test_hub_dot_speaker_dot,
+    test_hub_dot_system_dot,
+)
